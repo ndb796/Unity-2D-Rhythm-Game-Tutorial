85019dab6f92e555989afc94f3beb9d7d37cfe71
ndb796 <ndb796@naver.com>
2019-01-31 09:52:47 +0900

    구분선 작업 & 노트 오브젝트 생성 및 이동

---
+++ Assets/Scripts/NoteBehavior.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteBehavior : MonoBehaviour {
+
+    public int noteType;
+	
+	void Update () {
+        transform.Translate(Vector3.down * GameManager.instance.noteSpeed);
+	}
+}
